201d18c29fe3a5e7b01b47f1f11cf35066c9c3bd
alberto <albertozanola1996@gmail.com>
2021-07-14 17:27:35 +0200

    Generazione password

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var nome = prompt("inserisci il tuo nome")
+var cognome = prompt("inserisci il tuo cognome")
+var color = prompt("inserisci la tuo colore preferito")
+
+document .getElementById("password").innerHTML = nome + cognome + color + "21"
